a218dfa32464c64c4322f2e9e678844239c66585
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-02-15 17:48:09 -0600

    HTML

--- golesAjax.js
+++ golesAjax.js
@@ -3,7 +3,7 @@
 
 
 var spanObjetivo = "divTarget";
-var golesActuales = parseInt(document.getElementById("divTarget").innerText);
+var golesActuales = parseInt(document.getElementById("divTarget").innerHTML);
 while (golesActuales != 10) {
 
     var urlCompuesta, urlVariables = ("?id=" + id + "&cantidad=" + cantidad + "&operación=" + operación), uriPHP = "php scripts/AltasBajas.php";
