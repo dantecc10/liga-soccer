53a7ad23429f3f3d21abe7f2a12d81310fb7a60a
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-02-15 21:24:59 -0600

    ..

--- golesAjax.js
+++ golesAjax.js
@@ -8,16 +8,16 @@ function ajax() {
 
     var urlCompuesta;
     var urlVariables = "";// = ("?id=" + id + "&cantidad=" + cantidad + "&operación=" + operación), uriPHP = "php scripts/AltasBajas.php";
-  //  urlCompuesta = ("php scripts/muestraGoles.php?pedido=goles");
+    urlCompuesta = ("php scripts/muestraGoles.php?pedido=goles");
 
     //Petición AJAX
-    var xmlhttp = new XMLHttpRequest();
+/*     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById(spanObjetivo).innerHTML = this.responseText;
 
         }
-    };
+    }; */
 
     //Procesamiento AJAX
     xmlhttp.open("GET", urlCompuesta, true);
