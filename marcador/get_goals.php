c6e1864e09122a467ace94b8b5f149a1f60579ad
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-02-17 14:35:27 -0600

    ajax con intervalo

--- marcador/get_goals.php
+++ marcador/get_goals.php
@@ -28,27 +28,3 @@ if ($result->num_rows > 0) {
 
 $conn->close();
 ?>
-JS code to update the HTML span:
-
-javascript
-Copy code
-// AJAX request to update the HTML span with the number of goals
-function updateGoals() {
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-document.getElementById("total-goals").innerHTML = this.responseText;
-}
-};
-xhttp.open("GET", "get_goals.php", true);
-xhttp.send();
-}
-
-// Call the updateGoals function every 5 seconds
-setInterval(updateGoals, 5000);
-HTML code for the span that displays the number of goals:
-
-html
-Copy code
-<span id="total-goals"><?php echo $total_goals; ?></span>
-In this code, the PHP script fetches the total number of goals from the database and stores it in the $total_goals variable. The HTML span displays the value of $total_goals. The JavaScript code uses an AJAX request to fetch the updated value of $total_goals from the server every 5 seconds and updates the HTML span without reloading the page.
--- marcador/index.php
+++ marcador/index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script>// AJAX request to update the HTML span with the number of goals
+    <script>
+        // AJAX request to update the HTML span with the number of goals
         function updateGoals() {
             var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
+            xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
                     document.getElementById("total-goals").innerHTML = this.responseText;
                 }
@@ -16,11 +18,14 @@
             xhttp.open("GET", "get_goals.php", true);
             xhttp.send();
         }
-    
+
         // Call the updateGoals function every 5 seconds
-        setInterval(updateGoals, 5000);</script>
+        setInterval(updateGoals, 5000);
+    </script>
 </head>
+
 <body>
     <span id="total-goals"><?php echo $total_goals; ?></span>
 </body>
+
 </html>
