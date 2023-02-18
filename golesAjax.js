94ddf41bffe71c698b7425dc9955f20136641502
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-02-17 20:25:09 -0600

    quito tirador de Chrome

--- golesAjax.js
+++ golesAjax.js
@@ -3,29 +3,27 @@
 
 function ajax() {
     var spanObjetivo = "divTarget";
-    var terminar = false;
-    while (terminar == false) {
 
-        var urlCompuesta;
-        var urlVariables = "";// = ("?id=" + id + "&cantidad=" + cantidad + "&operación=" + operación), uriPHP = "php scripts/AltasBajas.php";
-        urlCompuesta = ("php scripts/muestraGoles.php?pedido=goles");
+    var urlCompuesta;
+    var urlVariables = "";// = ("?id=" + id + "&cantidad=" + cantidad + "&operación=" + operación), uriPHP = "php scripts/AltasBajas.php";
+    urlCompuesta = ("php scripts/muestraGoles.php?pedido=goles");
 
-        //Petición AJAX
-        var xmlhttp = new XMLHttpRequest();
-        /*     xmlhttp.onreadystatechange = function () {
-                 if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById(spanObjetivo).innerHTML = this.responseText;
-        
-                }
-            }; */
+    //Petición AJAX
+    var xmlhttp = new XMLHttpRequest();
+    /*     xmlhttp.onreadystatechange = function () {
+             if (this.readyState == 4 && this.status == 200) {
+                document.getElementById(spanObjetivo).innerHTML = this.responseText;
+    
+            }
+        }; */
 
-        //Procesamiento AJAX
-        xmlhttp.open("GET", urlCompuesta, true);
-        console.log("La cantidad actual de goles es de ");
-        console.log("URL: " + urlCompuesta + "\nURL Variables: " + urlVariables);
-        //console.log("ModoFiltro: " + ModoFiltro);
-        xmlhttp.send();
-    }
+    //Procesamiento AJAX
+    xmlhttp.open("GET", urlCompuesta, true);
+    console.log("La cantidad actual de goles es de ");
+    console.log("URL: " + urlCompuesta + "\nURL Variables: " + urlVariables);
+    //console.log("ModoFiltro: " + ModoFiltro);
+    xmlhttp.send();
+
 }
 
 setInterval(ajax, 5000);
