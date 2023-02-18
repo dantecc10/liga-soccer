76fbc91f371f862802e0bb10dbadb1dcecfe00b2
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2023-02-17 21:53:33 -0600

    nombre no

--- marcador/get_goals.php
+++ marcador/get_goals.php
@@ -7,24 +7,24 @@ $dbname = "live_stats";
 
 $conn = new mysqli("localhost", "liga-soccer", "soccerLeague22!!", "soccer_league");
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-// SQL query to get the sum of goals
-$sql = "SELECT SUM(goals) as total_goals FROM player_stats";
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Output data of each row
-    while ($row = $result->fetch_assoc()) {
-        $total_goals = $row["total_goals"];
-    }
-} else {
-    $total_goals = 0;
-}
-
-$conn->close();
-?>
+//// Check connection
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
+//
+//// SQL query to get the sum of goals
+//$sql = "SELECT SUM(goals) as total_goals FROM player_stats";
+//
+//$result = $conn->query($sql);
+//
+//if ($result->num_rows > 0) {
+//    // Output data of each row
+//    while ($row = $result->fetch_assoc()) {
+//        $total_goals = $row["total_goals"];
+//    }
+//} else {
+//    $total_goals = 0;
+//}
+//
+//$conn->close();
+//
